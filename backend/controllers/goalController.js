3e46f2ccfeb971267db209800c64f1b7bd59702c
fkolender <hedrite@hotmail.com>
2023-03-17 14:23:58 -0300

    Mongoose connected to MongoDB

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,39 +1,41 @@
+const asyncHandler = require('express-async-handler')
+
 // desc - Get goals
 // route - GET /api/goals
 // access - private
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 // desc - Update goal 
 // route - PUT /api/goals
 // access - private
 
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Updated goal ${req.params.id}`})
-}
+})
 
 // desc - Create goal
 // route - POST /api/goals
 // access - private
 
-const createGoals = (req, res) => {
+const createGoals = asyncHandler(async (req, res) => {
     console.log(req.body)
     if (!req.body.text){
         res.status(400)
         throw new Error('Please add a field to create the goal')
     }
     res.status(200).json({message: `Create goal`})
-}
+})
 
 // desc - Delete goal
 // route - DELETE /api/goals
 // access - private
 
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Deleted goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals, setGoals, createGoals, deleteGoals
